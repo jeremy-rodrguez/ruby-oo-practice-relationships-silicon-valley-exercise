e148fc7916facf84b4678aea61e39c06acdc9b77
Jeremy <jeremy.rodrguez@yahoo.com>
2020-11-20 12:01:10 -0800

    Ange and Jeremy's Doing

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,29 @@
 class Startup
 
+    attr_accessor :name, :domain, :pivot
+    attr_reader :founder
+
+    @@all = []
+    
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @pivot = pivot
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(founder)
+        Startup.all.find { |startup|
+            startup.founder == founder
+        }
+    end
+
+    def self.domains
+        domain.self
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,7 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+s1 = Startup.new("rose tech", "Rose", "Searching")
+s2 = Startup.new("rose techz", "Roseann", "Not Searching")
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
